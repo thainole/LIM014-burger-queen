4ae7017f74fec96acc5f7748c6c6254a93af7a86
meryvera <yrem.vera.5@gmail.com>
2021-05-27 12:03:51 -0500

    Doing sass home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,9 @@ import mainBurger from "../img/burger.png"
 export const Inicio = () => {
   return (
     <section className="home">
-      <h1 className="h1">Bienvenido al sistema de pedidos de Burger Queens</h1>
-      <article className="">
+      <h1 className="home__title">Bienvenido al sistema de pedidos de Burger Queens</h1>
+      <article className="home__article">
+        <p>holaaaaa</p>
         <figure>
           <img className="" src={mainBurger} width="100px" alt="burger" />
         </figure>
