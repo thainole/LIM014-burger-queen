673ce805fe4dc93e44224b961367a81a2b044e54
meryvera <yrem.vera.5@gmail.com>
2021-05-28 12:43:11 -0500

    Doing Sass v2

--- src/components/Server/Order.jsx
+++ src/components/Server/Order.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavBar } from './NavBar'
 import { Products } from './Products'
+import { OrderList } from './OrderList'
 
 export const Order = () => {
   return (
@@ -8,7 +9,7 @@ export const Order = () => {
       <NavBar />
       <article className="orderContainer">
         <Products />
-        <p>hola soy la orden</p>
+        <OrderList />
       </article>
     </section>
   )
