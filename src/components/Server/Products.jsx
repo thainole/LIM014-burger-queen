d65fbbcc56a06510a9e7ca32e3d657dc18d4c4a0
thainole <thainole@gmail.com>
2021-05-28 19:31:53 -0500

    rendering products

--- src/components/Server/Products.jsx
+++ src/components/Server/Products.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import data from './data.json'
 
 export const Products = () => {
   return (
-    <>
+    <article className="productsList">
       <nav>
-{/*         <li><Link></Link></li> */}
-        <li></li>
-        <li></li>
+        <ul>
+          <div>
+            <li>Desayuno</li>
+            <li>Hamburguesas</li>
+            <li>Acompañamientos</li>
+            <li>Bebidas</li>
+          </div>
+        </ul>
       </nav>
-    </>
+      <article>
+      <ul className="cards">
+        {data.map(p => (
+          <div>
+            <img src={p.img} alt="" />
+            <li key={`p--${p.id}`}>{p.name}</li>
+            <p>S/. {p.price}</p>  
+          </div>  
+        ))}
+        </ul>
+      </article>
+    </article>
   )
+
 }
