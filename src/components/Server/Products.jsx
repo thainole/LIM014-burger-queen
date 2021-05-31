9a1d8b58113512c22187678c4b8865babd03037a
thainole <thainole@gmail.com>
2021-05-30 20:23:33 -0500

    layout products+order list

--- src/components/Server/Products.jsx
+++ src/components/Server/Products.jsx
@@ -1,26 +1,53 @@
-import React from 'react'
-import data from './data.json'
+import React, { useState } from 'react'
+import dataJson from './data.json'
 
 export const Products = () => {
+  const data = dataJson.products;
+
+  const firstView = data.filter((elem) => elem.type === 'Desayuno');
+  const [products, setProducts] = useState(firstView);
+
+  const productsType = (option) => {
+    // eslint-disable-next-line default-case
+    switch (option) {
+      case 'Desayuno':
+        const breakfast = data.filter((elem) => elem.type === option);
+        setProducts(breakfast);
+        break;
+      case 'Hamburguesas':
+        const burgers = data.filter((elem) => elem.type === option);
+        setProducts(burgers);
+        break;
+      case 'Acompañamientos':
+        const sides = data.filter((elem) => elem.type === option);
+        setProducts(sides);
+        break;
+      case 'Bebidas':
+        const drinks = data.filter((elem) => elem.type === option);
+        setProducts(drinks);
+        break; 
+    }
+  }
   return (
     <article className="productsList">
       <nav>
         <ul>
           <div>
-            <li>Desayuno</li>
-            <li>Hamburguesas</li>
-            <li>Acompañamientos</li>
-            <li>Bebidas</li>
+            <li onClick={() => productsType('Desayuno')}>Desayuno</li>
+            <li onClick={() => productsType('Hamburguesas')}>Hamburguesas</li>
+            <li onClick={() => productsType('Acompañamientos')}>Acompañamientos</li>
+            <li onClick={() => productsType('Bebidas')}>Bebidas</li>
           </div>
         </ul>
       </nav>
       <article>
       <ul className="cards">
-        {data.map(p => (
+        {products.map(p => (
           <div>
             <img src={p.img} alt="" />
-            <li key={`p--${p.id}`}>{p.name}</li>
-            <p>S/. {p.price}</p>  
+            <li key={p.id}>{p.name}</li>
+            <p>S/. {p.price}</p>
+            <button>Agregar</button>
           </div>  
         ))}
         </ul>
