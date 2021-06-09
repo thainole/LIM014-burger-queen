fca757a2f26b69846eb81deaf6dee7754557eceb
thainole <thainole@gmail.com>
2021-06-09 15:03:01 -0500

    starting chef orders with firestore

--- src/components/Chef/ChefOrder.jsx
+++ src/components/Chef/ChefOrder.jsx
@@ -1,31 +1,29 @@
 import React from 'react'
+import { readAllOrders } from '../../firebase/firestore'
+import { ChefEachOrder } from './ChefEachOrder';
 import { ChefNavBar } from './ChefNavBar';
 
 export const Chef = () => {
+
+  const [orders, setOrders] = React.useState([])
+
+  const readOrders = () => {
+    setOrders(readAllOrders) 
+  }
+  console.log(setOrders(readAllOrders));
+
+  React.useEffect(() => {
+    readOrders()
+  }, [])
+
   return (
     <section>
       <ChefNavBar />
       <article className="statusCard">
-          <div className="cards">
-            <h3>Order #1234</h3>
-            <div>
-              <div className="onlyrow">
-                <p><strong>1&nbsp;&nbsp;</strong></p>
-                <p>Hamburguesa simple de pollo con huevo</p>
-              </div>
-              <div className="onlyrow">
-                <p><strong>1&nbsp;&nbsp;</strong></p>
-                <p>Hamburguesa simple de pollo con huevo</p>
-              </div>
-            </div>
-            <button>Orden lista para entregar</button>
-            <div className="smallerFont">
-              <p>Hora del pedido:</p>
-              <p>Tiempo: *que salga corriendo*</p>
-            </div>
-              
-            </div>
-          </article>
+        {orders.map(order => (
+          <ChefEachOrder order={order} key={order.id}/>
+        ))}
+      </article>
     </section>
     
   )
--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -1,8 +1,14 @@
 import { db } from './config'
-console.log(db);
 
-export const createOrder = (estado) => {
-  db.collection('orders').add({
-    estado
-  })
+export const createOrder = (orderData) => db.collection('orders').add(orderData)
+
+export const readAllOrders = () => {
+  db.collection('orders')
+    .onSnapshot((querySnapshot) => {
+      const prueba = querySnapshot.docs.map((doc) => ({
+        orderId: doc.id,
+        ...doc.data(),
+      }))
+      console.log(prueba)
+    });
 }
