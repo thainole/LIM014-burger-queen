e50d617254899753e5baddcfdae6a8396447852c
thainole <thainole@gmail.com>
2021-06-13 21:38:47 -0500

    feat orders history chef and server

--- src/components/Chef/ChefHistory.jsx
+++ src/components/Chef/ChefHistory.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import { CardsHistory } from '../Server/CardsHistory'
 import { ChefNavBar } from './ChefNavBar'
+import { readAllOrders } from '../../firebase/firestore'
 
 export const ChefHistory = () => {
+
+  const [ordersH, setOrdersH] = React.useState([]);
+
+  React.useEffect(() => {
+    readAllOrders(setOrdersH);
+  }, []);
+
   return (
     <section>
       <ChefNavBar />
-      <CardsHistory />
+      <article className="statusCard">
+        {ordersH.length > 0
+          ? ordersH.map((orderH) => (
+              <CardsHistory
+                orderH={orderH}
+                key={orderH.id}
+              />
+            ))
+          : null
+        }
+      </article>
     </section>
   )
 }
